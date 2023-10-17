7d61cc3a19794e6c5bdeb2b56cd0bee7b629b5b8
Nate Maile <npmaile@gmail.com>
2023-10-17 19:08:59 -0400

    init commit

---
+++ mdparser.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+enum mdAttr { MDbold, MDitalic, MDmonospace, MDh1, MDh2, MDh3 };
+
+typedef struct mdtext {
+  char *text;
+  int textlen;
+  enum mdAttr attribute;
+} mdText;
+
+typedef struct mdDoc {
+  struct mdtext *texts;
+} mdDoc;
+
+int main() {
+  FILE *fp;
+  fp = fopen("testfile.md", "r");
+  int c;
+  if (fp == NULL) {
+    perror("Error opening file");
+    return (-1);
+  }
+  mdDoc *document;
+  document = malloc(sizeof(&document));
+  do {
+    c = fgetc(fp);
+    if (feof(fp)) {
+      break;
+    }
+
+  } while (1);
+  fclose(fp);
+  return (0);
+}
